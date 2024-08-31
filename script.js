3477a767b3ab2a419e363152c35324ceed3065ee
Ahsan Sajjad <princeahsan0608@gmail.com>
2024-09-01 01:45:59 +0500

    Animations and products add ..

--- script.js
+++ script.js
@@ -7,44 +7,195 @@ const navLink = document.getElementById("nav-links")
 const cart = document.getElementById("cart")
 const cartClose = document.getElementById("cart-close-icon")
 const menuIcon = document.getElementById("menu-icon")
+const alllink = navLink.querySelectorAll("a")
+const allproductcontainer = document.getElementById("all-products")
 
-menuBtn.addEventListener('click', e => {
+// ================================================================================
+// Data
+// ================================================================================
 
-    if (!navLink.classList.contains('show')) {
+
+let allproduct =[
+{
+    title : "Brown purse",
+    category : "Bag",
+    price : 100,
+    thumbnail : "./Assets/bag 1.jpg"
+},
+{
+    title : "Camera",
+    category : "Electronics",
+    price : 200,
+    thumbnail : "./Assets/camera 1.jpg"
+},
+{
+    title : "Lense",
+    category : "Gadgets",
+    price : 200,
+    thumbnail : "./Assets/lense 1.jpg"
+},
+{
+    title : "Brown purse",
+    category : "Bag",
+    price : 100,
+    thumbnail : "./Assets/bag 2.jpg"
+},
+{
+    title : "Camera",
+    category : "Electronics",
+    price : 200,
+    thumbnail : "./Assets/camera 2.jpg"
+},
+{
+    title : "Lense",
+    category : "Gadgets",
+    price : 200,
+    thumbnail : "./Assets/lense 2.jpg"
+},
+{
+    title : "Brown purse",
+    category : "Bag",
+    price : 100,
+    thumbnail : "./Assets/bag 3.jpg"
+},
+{
+    title : "Camera",
+    category : "Electronics",
+    price : 200,
+    thumbnail : "./Assets/camera 3.jpg"
+},
+{
+    title : "Lense",
+    category : "Gadgets",
+    price : 200,
+    thumbnail : "./Assets/lense 3.jpg"
+},
+{
+    title : "Brown purse",
+    category : "Bag",
+    price : 100,
+    thumbnail : "./Assets/bag 4.jpg"
+},
+{
+    title : "Camera",
+    category : "Electronics",
+    price : 200,
+    thumbnail : "./Assets/camera 4.jpg"
+},
+{
+    title : "Lense",
+    category : "Gadgets",
+    price : 200,
+    thumbnail : "./Assets/lense 4.jpg"
+},
+
+
+
+
+
+]
+
+
+
+
+// ================================================================================
+// Functions
+// ================================================================================
+
+
+
+const updateAllProductDom = ()=>{
+    allproductcontainer.innerHTML = `
+    ${
+        allproduct.map((product)=>{
+
+            return `<div class="product-card">
+
+              <div class="product-img">
+              <img src="${product.thumbnail}" alt="bag">
+              </div>
+
+              <div class="product-details">
+                  <h2>${product.title}</h2>
+                  <small>Category : ${product.category}</small>
+                  <p>price : ${product.price} Pkr</p>
+                  </div>
+                  
+                  </div>`
+                  
+                }).join("")
+            }
+            `
+            
+            
+        }
         
-        navLink.classList.add('show');
-        menuIcon.className = "fa-solid fa-square-xmark"
-    }
-    
-    else {
         
-        navLink.classList.remove('show')
-        menuIcon.className = "fa-solid fa-bars"
-    }
-})
-
-// ----------------- endNav
-
-
-
-
-// ----------------- Cart
-cartBtn.addEventListener('click', e => {
-    
-    if (!cart.classList.contains('show')) {
         
-        cart.classList.add('show');
         
-    }
-    
-    else {
         
-        cart.classList.remove('show')
+        // ================================================================================
+        // All Event Listener
+        // ================================================================================
         
-    }
+        menuBtn.addEventListener('click', e => {
+            
+            if (!navLink.classList.contains('show')) {
+                
+                navLink.classList.add('show');
+                menuIcon.className = "fa-solid fa-square-xmark"
+            }
+            
+            else {
+                
+                navLink.classList.remove('show')
+                menuIcon.className = "fa-solid fa-bars"
+            }
+        })
+        
+        alllink.forEach(link => {
+            
+            
+            link.addEventListener('click', ()=>{
+                navLink.classList.remove('show')
+                menuIcon.className = "fa-solid fa-bars"
+                
+            })
+            
+        })
+        
+        // ----------------- endNav
+        
+        
+        
+        
+        // ----------------- Cart
+        cartBtn.addEventListener('click', e => {
+            
+            if (!cart.classList.contains('show')) {
+                
+                cart.classList.add('show');
+                
+            }
+            
+            else {
+                
+                cart.classList.remove('show')
+                
+            }
 })
 
 cartClose.addEventListener('click',()=>{
     cart.classList.remove('show')
 })
-    // ----------------- Cart END
+// ----------------- Cart END
+
+
+// ----------------- Product
+
+// ================================================================================
+// INIT
+// ================================================================================
+updateAllProductDom()
+
+
